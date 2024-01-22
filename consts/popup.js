1202bcffcd73bbb98df5d3bd95503af881c991ea
juhan00 <jvabya@gmail.com>
2024-01-22 22:29:42 +0900

    fix: rm -r --cached .

--- consts/popup.js
+++
@@ -1,8 +0,0 @@
-export const POPUP_DEFAULT = "DEFAULT";
-export const POPUP_CONTACTUS = "CONTACTUS";
-export const POPUP_COST = "COST";
-export const POPUP_PLATFORM = "PLATFORM";
-
-export const POPUP_AGREE_TYPE = "DEFAULT";
-export const POPUP_AGREE1_TYPE = "AGREE1";
-export const POPUP_AGREE2_TYPE = "AGREE2";
--- next-i18next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  i18n: {
-    locales: ["en", "ko"],
-    defaultLocale: "ko",
-    localeDetection: false,
-  },
-};
