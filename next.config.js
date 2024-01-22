a67a9bdff5e6b5ec6e4087ce252ae875f356f78c
juhan00 <jvabya@gmail.com>
2024-01-22 19:43:42 +0900

    feat: next.config.js 수정

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
     return [
       {
         source: "/:path*",
-        destination: `${process.env.NEXT_PUBLIC_DEFAULT_API_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_DEFAULT_API_URL}:path*`,
       },
     ];
   },
